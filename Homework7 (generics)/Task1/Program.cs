fe025fa784e9a6ce7c7ebea7edabe84859e92c6e
QV1LL <ghost228678@gmail.com>
2025-03-10 17:31:30 +0100

    Init homework 7 and done tasks 1 to 6

---
+++ Homework7 (generics)/Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿using Task1To6.Shared;
+
+namespace Task1;
+
+internal static class Program
+{
+    static void Main(string[] args)
+    {
+        // Is fibonacci number
+        Console.WriteLine(4.IsFibonacci());
+
+        // Get count of words
+        Console.WriteLine("Some text for test".GetCountOfWords());
+
+        // Get len of last word
+        Console.WriteLine("Some text for test".GetLengthOfLastWords());
+
+        // Check if exists substring in text
+        Console.WriteLine("Some text for test".CheckForSubstring("text"));
+
+        // Check correctless of brackets
+        Console.WriteLine("({[]})".IsValidBrackets());
+    }
+}
---
+++ Homework7 (generics)/Task1/Shared/MyExtensions.cs
@@ -0,0 +1,49 @@
+﻿namespace Task1To6.Shared;
+
+internal static class MyExtensions
+{
+    public static bool IsFibonacci(this int number)
+    {
+        return IsPerfectSquare(5 * number * number + 4) || 
+               IsPerfectSquare(5 * number * number - 4);
+    }
+
+    public static int GetCountOfWords(this string text) => text.Split(" ").Count();
+
+    public static int GetLengthOfLastWords(this string text) => text.Split(" ")[0].Length;
+
+    public static bool CheckForSubstring(this string text, string substring) => text.Contains(substring);
+
+    public static bool IsValidBrackets(this string input)
+    {
+        Stack<char> brackets = new Stack<char>();
+
+        Dictionary<char, char> bracketPairs = new Dictionary<char, char>
+        {
+            { ')', '(' },
+            { '}', '{' },
+            { ']', '[' }
+        };
+
+        foreach (char c in input)
+        {
+            if (c == '(' || c == '{' || c == '[')
+                brackets.Push(c);
+
+            else if (c == ')' || c == '}' || c == ']')
+                if (brackets.Count == 0 || brackets.Pop() != bracketPairs[c])
+                    return false;
+        }
+
+        return brackets.Count == 0;
+    }
+
+    public static IEnumerable<T> GetSorted<T>(this IEnumerable<T> values, Predicate<T> predicate)
+        => values.OrderBy(new Func<T, bool>(predicate));
+
+    private static bool IsPerfectSquare(long x)
+    {
+        long sqrt = (long)Math.Sqrt(x);
+        return sqrt * sqrt == x;
+    }
+}
