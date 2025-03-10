9b23638b54e38d230cc447e11699a6fe41841b4a
QV1LL <ghost228678@gmail.com>
2025-03-10 18:44:36 +0100

    Done task 7

---
+++ Homework7 (generics)/Task7/Program.cs
@@ -0,0 +1,23 @@
+﻿using Task7.ValueObjects;
+
+namespace Task7;
+
+internal static class Program
+{
+    static void Main(string[] args)
+    {
+        Random random = new();
+        DayTemperature[] temperatures = new DayTemperature[10];
+
+        for (int i = 0; i < temperatures.Length; i++)
+        {
+            float minTemp = (float)(random.NextDouble() * 30 + 273);
+            float maxTemp = (float)(random.NextDouble() * 20 + minTemp); 
+
+            temperatures[i] = new DayTemperature(maxTemp, minTemp);
+        }
+
+        Array.Sort(temperatures, (a, b) => b.Difference.CompareTo(a.Difference));
+        Console.WriteLine($"The max difference: {temperatures[0].Difference}");
+    }
+}
---
+++ Homework7 (generics)/Task7/ValueObjects/DayTemperature.cs
@@ -0,0 +1,30 @@
+﻿namespace Task7.ValueObjects;
+
+internal record class DayTemperature(float maxTemperature, float minTemperature)
+{
+    public float MaxTemperature
+    {
+        get => maxTemperature;
+        init
+        {
+            if (value < 273)
+                throw new ArgumentOutOfRangeException("Uncorrect temperature");
+
+            maxTemperature = value;
+        }
+    }
+
+    public float MinTemperature
+    {
+        get => minTemperature;
+        init
+        {
+            if (value < 273)
+                throw new ArgumentOutOfRangeException("Uncorrect temperature");
+
+            minTemperature = value;
+        }
+    }
+
+    public float Difference { get => Math.Abs(maxTemperature - minTemperature); }
+}
