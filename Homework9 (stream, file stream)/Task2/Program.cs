1e4ecf2dad13e367ea56ccc06384f0067f0df802
QV1LL <ghost228678@gmail.com>
2025-03-17 17:12:21 +0100

    Added task 1

---
+++ Homework9 (stream, file stream)/Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Task2;
+
+internal static class Program
+{
+    private static string _filePath = "text.txt";
+
+    public static void Main(string[] args)
+    {
+        string? text = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), _filePath));
+        
+        File.WriteAllText(Path.Combine(Directory.GetCurrentDirectory(), _filePath), ReplaceWordInText(text, "the", "***"));
+    }
+
+    private static string? ReplaceWordInText(string? text, string targetWord, string replaceWord)
+    {
+        if (text == null) return text;
+
+        int occurence = text.Split(" ").Count(w => w == targetWord);
+        Console.WriteLine("Count of replaced words in text: " + occurence);
+
+        return text.Replace(targetWord, replaceWord);
+    }
+}
